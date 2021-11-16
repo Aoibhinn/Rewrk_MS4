94280f7d6e4a3717cc5c65fc9ffbcec6f8123fb9
Aoibhinn <97aoibhinn@gmail.com>
2021-11-16 23:13:01 +0000

    create view code for service app, base.html and service.html file

--- service/views.py
+++ service/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Service
 
-# Create your views here.
+
+class ServiceList(generic.ListView):
+    model = Service
+    context_object_name = 'services'
+    queryset = Service.objects.filter(status=1).order_by('-created_on')
+    template_name = 'services.html'
+
+ 
