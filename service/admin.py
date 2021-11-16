d9511415362cf1ad6a94acdddab0c2628b534f8a
Aoibhinn <97aoibhinn@gmail.com>
2021-11-16 22:00:17 +0000

    add additional functionality to admin service

--- service/admin.py
+++ service/admin.py
@@ -5,4 +5,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Service)
 class ServiceAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('service_name',)}
+    list_display = ('service_name', 'slug', 'status', 'price')
+    search_fields = ['service_name', 'employee']
+    list_filter = ('status', 'created_on')
     summernote_fields = ('description')
