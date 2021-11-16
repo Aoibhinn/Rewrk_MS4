80c202e13d83b94d166be46b442380a1de5d2060
Aoibhinn <97aoibhinn@gmail.com>
2021-11-16 21:46:26 +0000

    add summernote and add it to the description field for service

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
