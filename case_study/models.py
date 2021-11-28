f52e384e6c82eee7c0c7e4985f844d38fa2696d6
Aoibhinn <97aoibhinn@gmail.com>
2021-11-28 09:27:25 +0000

    create case study model

--- case_study/models.py
+++ case_study/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    return self.title
