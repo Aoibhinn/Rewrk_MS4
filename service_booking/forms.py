3727f176694c33ac72d6697af98b4e34d47ddaa6
Aoibhinn <97aoibhinn@gmail.com>
2021-12-05 13:02:57 +0000

    change form field from date to additional information

--- service_booking/forms.py
+++ service_booking/forms.py
@@ -10,4 +10,5 @@ class BookingForm(forms.Form):
     telephone = forms.IntegerField(required=True)
     service = forms.ModelChoiceField(
         queryset=Service.objects.filter(status=1), initial=0)
-    date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))                   
+    additional_information = forms.CharField(
+        widget=forms.Textarea, required=False)              
