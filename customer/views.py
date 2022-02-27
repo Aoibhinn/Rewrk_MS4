50318b3e3f99c2cfd2c02078a582abc03f884133
Aoibhinn <97aoibhinn@gmail.com>
2022-02-27 10:35:45 +0000

    Add success url and message to create booking view

--- customer/views.py
+++ customer/views.py
@@ -4,6 +4,8 @@
 """
 from django.views import generic
 from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from .models import Booking
 from .forms import CreateBookingForm
 
@@ -19,11 +21,14 @@ class BookingView(generic.ListView):
         return booking_list
 
 
-class CreateBookingView(CreateView):
+class CreateBookingView(SuccessMessageMixin, CreateView):
     """Create new booking view"""
     model = Booking
     form_class = CreateBookingForm
     template_name = 'create_booking.html'
+    success_url = reverse_lazy('booked_services')
+    success_message = 'Thank you for booking with us,\
+                        a member of our team will reach out to you shortly!'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
