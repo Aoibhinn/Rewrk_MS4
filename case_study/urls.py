18d8a2f18a165cfb08aa23357af1134a8e0e55b5
Aoibhinn <97aoibhinn@gmail.com>
2022-02-27 17:18:36 +0000

    Create url and view for liking a post, and else if statement for like when user is authenticated

--- case_study/urls.py
+++ case_study/urls.py
@@ -8,4 +8,5 @@ from django.urls import path
 urlpatterns = [
     path('case_study/', views.PostList.as_view(), name='case_study'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
