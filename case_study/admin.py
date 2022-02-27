74f58618750494b484f2cdc6bbc4bfb790bed3c6
Aoibhinn <97aoibhinn@gmail.com>
2022-02-27 19:36:00 +0000

    Bug Fixes: Flint 8 checnages for admin and app case_study

--- case_study/admin.py
+++ case_study/admin.py
@@ -1,22 +1,36 @@
+"""
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """
+    Post Fields for display and filter
+    """
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on', 'employee')
     summernote_fields = ('content')
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Comment Fields for display and filter
+    """
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
+        """
+        Field to approve comments
+        """
         queryset.update(approved=True)
--- case_study/apps.py
+++ case_study/apps.py
@@ -6,6 +6,8 @@ from django.apps import AppConfig
 
 
 class CaseStudyConfig(AppConfig):
-    
+    """
+    Case Study App
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'case_study'
