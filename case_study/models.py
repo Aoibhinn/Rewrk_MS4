324e0391e4ab566af317ddf99027340b1925c663
Aoibhinn <97aoibhinn@gmail.com>
2022-02-27 16:35:53 +0000

    Bug Fix: add likes to post model

--- case_study/models.py
+++ case_study/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
+    """
+    Post/Case Study Model
+    """
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -11,14 +16,19 @@ class Post(models.Model):
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
+    class Meta:
+        ordering = ["-created_on"]
 
-class Meta:
-    ordering = ['-created_on']
+    def __str__(self):
+        return self.title
 
+    def number_of_likes(self):
+        return self.likes.count()
 
-def __str__(self):
-    return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
